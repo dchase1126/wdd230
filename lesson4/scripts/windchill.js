43516fa078412c48b45b0f9121c3faa22d58c296
dchase1126 <cha20068@byui.edu>
2023-02-09 18:40:52 -0700

    updated wind chill js and updated html

---
+++ lesson4/scripts/windchill.js
@@ -0,0 +1,11 @@
+const s = 10;
+document.getElementById('wind_speed').innerText = s
+
+const t = 32;
+document.getElementById('temp').innerText = t
+if 
+
+else
+const windchill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275*t * (s ** .016));
+document.getElementById("wind_chill").innerText = windchill
+
