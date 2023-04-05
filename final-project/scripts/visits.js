abe8ff9e8f963a0c280ab2e73ab9ec311f4581a5
Jacksonckc <jackson858216047@gmail.com>
2023-04-05 14:56:38 -0600

    changes made to show order confirmation

--- final-project/scripts/visits.js
+++ final-project/scripts/visits.js
@@ -1,23 +1,17 @@
-const lastVisit = document.getElementById("lastVisit");
-const numberVisit = document.getElementById("numberVisit");
-const timestamp = Date.now()
-
+const lastVisit = document.getElementById('lastVisit');
+const numberVisit = document.getElementById('numberVisit');
+const timestamp = Date.now();
 
 // get the timestamp from localstorage,
-const time2 = localStorage.getItem("lastVisitTimestamp")
-const numvisit = localStorage.getItem("numberVisitTimestamp")
+const time2 = localStorage.getItem('lastVisitTimestamp');
+const numvisit = localStorage.getItem('numberVisitTimestamp');
 // now - timestamp in localstorage
 // difference/(1000*60*60*24) - how many mil sec in a day
-const time = ((timestamp - time2) / (1000 * 60 * 60 * 24))
+const time = (timestamp - time2) / (1000 * 60 * 60 * 24);
 
-localStorage.setItem("lastVisitTimestamp", timestamp)
-localStorage.setItem("numberVisitTimestamp", timestamp)
-console.log(time2)
+localStorage.setItem('lastVisitTimestamp', timestamp);
+localStorage.setItem('numberVisitTimestamp', timestamp);
 // put the difference here
 
-lastVisit.innerText = time.toFixed(0) 
-numberVisit.innerText = time.toFixed(0) 
-
-
-
-
+lastVisit.innerText = time.toFixed(0);
+numberVisit.innerText = time.toFixed(0);
